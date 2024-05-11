edfedf7ebc494139e2a4822864a19c2c4675c644
TabrizPlv <tabrizpahlavi@gmail.com>
2024-05-12 00:41:42 +0800

    stage 3

--- app/server.go
+++ app/server.go
@@ -14,18 +14,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 		go handleConnection(conn)
 	}
-	conn.Close()
 }
 
 func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
 	var response string = "+PONG\r\n"
 	_, err := conn.Write([]byte(response))
 	if err != nil {
